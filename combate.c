1903e823b5e86c369e779280ea13838b0ffb4851
JPLoso <pedrovelosoj5@gmail.com>
2025-06-24 10:12:27 -0300

    commit do combate inacabado

--- combate.c
+++ combate.c
@@ -20,26 +20,28 @@ Ataque ataques[4] = {
 };
 
 
+int telaAtaques(){
+    int opc = menu("                      ", 1, 4, ataques->nome[0], ataques->nome[1], ataques->nome[2], ataques->nome[3]);
+    return opc;
+}
+
+int telaInventario(int *inv){
+    int opc = menu("                      ", 1, 2, "Pocao de Cura", "Pocao de Mana");
+    return opc;
+}
+
+
 int combate(){
     int opc;
+    int inv[2] =   {5, 5}; 
     while(1){
         opc = menu("                      ", 1, 2, "Lutar", "Inventario");
         if(opc = 1){
             telaAtaques();
         }else if(opc = 2){
-            telaInventario();
+            telaInventario(inv);
         }
-
     }
      
-    return 1;
-}
-
-int telaAtaques(){
-    int opc = menu("                      ", 1, 4, ataques->nome[0], ataques->nome[1], ataques->nome[2], ataques->nome[3]);
-    return 1;
-}
-
-int telaInventario(){
     return 1;
 }
