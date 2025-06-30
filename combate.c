39cecf945012fd08fbf2d937a87059f7ab24cf2e
JPLoso <pedrovelosoj5@gmail.com>
2025-06-30 10:27:54 -0300

    implementacao da busca linear no inv

--- combate.c
+++ combate.c
@@ -21,16 +21,18 @@ int telaInventario(int *inv){
 }
 
 int combate(int escolha, int inimigo){
-    //status do jogador, e inimigo; 
-    int opc, atk, item;
+    int opc, atk = 0, item;
     int hpPlayer = classes[escolha].HP;
     int hpInimigo = inimigos[inimigo].HP;
     int ppPlayer = classes[escolha].PP;
     
     int inv[2] = {5, 5}; 
+    int itensDisponiveis[2] = {0, 1}; // IDs dos itens possíveis
+
     while(1){
         printf("\33[2K\rVida Player: %d - PP: %d | Vida Inimigo: %d\n", hpPlayer, ppPlayer, hpInimigo);
         opc = menu("                      ", 1, 2, "Lutar", "Inventario");
+
         if(opc == 1){
             atk = telaAtaques(escolha);
             if(ppPlayer > habilidades[(atk-1)+(4*escolha)].custo_pp){
@@ -38,29 +40,32 @@ int combate(int escolha, int inimigo){
                 ppPlayer -= habilidades[(atk-1)+(4*escolha)].custo_pp;
             }
             if (hpInimigo < 0) hpInimigo = 0;
-        }else if(opc == 2){
+        } else if(opc == 2){
             item = telaInventario(inv);
-            if(item == 0){
+            int indiceItem = buscaLinear(itensDisponiveis, 2, item); // busca linear aplicada aqui
+
+            if(indiceItem == 0 && inv[0] > 0){
                 hpPlayer += 50;
                 if (hpPlayer > classes[escolha].HP) hpPlayer = classes[escolha].HP;
                 inv[0] -= 1;
-            }else if(item == 1){
+            } else if(indiceItem == 1 && inv[1] > 0){
                 ppPlayer += 5;
                 if (ppPlayer > classes[escolha].PP) ppPlayer = classes[escolha].PP;
                 inv[1] -= 1;
             }
         }
+
         if(hpPlayer < 0) hpPlayer = 0;
         if(ppPlayer < 0) ppPlayer = 0;
+
         if(habilidades[(atk-1)+(4*escolha)].stun != 1){
             hpPlayer -= habilidadesInimigos[aleatorio(3)+(3*inimigo)].dano;
-
         }
-        
+
         if(hpPlayer == 0){
             return 0;
             break;
-        }else if(hpInimigo == 0){
+        } else if(hpInimigo == 0){
             return 1;
             break;
         }   
